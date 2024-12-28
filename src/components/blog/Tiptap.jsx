3638898655f5f5b02714ed225ae881fb2c3ea3e7
Kingakadam <aryankadam2952005@gmail.com>
2024-12-28 23:04:46 +0530

    initialcommit

---
+++ src/components/blog/Tiptap.jsx
@@ -0,0 +1,112 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+import '../index.css'
+const extensions = [StarterKit];
+
+const content = ``;
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+        >
+          Bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is-active" : ""}
+        >
+          Italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is-active" : ""}
+        >
+          Strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={!editor.can().chain().focus().toggleCode().run()}
+          className={editor.isActive("code") ? "is-active" : ""}
+        >
+          Code
+        </button>
+
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive("paragraph") ? "is-active" : ""}
+        >
+          Paragraph
+        </button>
+        {[1, 2, 3, 4, 5, 6].map((level) => (
+          <button
+            key={level}
+            onClick={() => editor.chain().focus().toggleHeading({ level }).run()}
+            className={editor.isActive("heading", { level }) ? "is-active" : ""}
+          >
+            H{level}
+          </button>
+        ))}
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          Bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+        >
+          Ordered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive("codeBlock") ? "is-active" : ""}
+        >
+          Code block
+        </button>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        >
+          Undo
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        >
+          Redo
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setColor("#958DF1").run()}
+          className={
+            editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+          }
+        >
+          Purple
+        </button>
+      </div>
+
+      <EditorContent editor={editor} />
+      <button onClick={() => console.log(editor.getJSON())}>Save</button>
+    </div>
+  );
+};
+
+export default Tiptap;
